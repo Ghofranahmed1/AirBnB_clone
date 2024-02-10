badeef5a56c3b393ae57dbfa73787462c3dcf176
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-02-10 11:24:02 -0800

    tasl base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,33 @@
 import cmd
 from uuid import uuid4
-from datetime import datatime
+from datetime import datetime
 
 
 """Class BaseModel that defines
 all common attributes/methods for other classes"""
 
 class BaseModel:
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         """Initialize Public instance attribute"""
-        self.id = map(str, uuid4())
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.iems():
+                if k == "created_at" or k == "updated_at":
+                    """here we prais the time  (today) to be in 
+                    time_format"""
+                    self.__dict__[k] = datatime.strptime(v, time_format)
+                else:
+                    self.__dict__[k] = v
+        """else:
+            models.storage.new(self)"""
 
     def save(self):
         """ updates the public instance attribute
         updated_at with the current datetime"""
-        self.updated_at = datatime.today()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """returns a dictionary containing all 
@@ -26,8 +37,9 @@ class BaseModel:
         effect the orignal dic, this function is like 
         create my own dic function"""
         
-        my_dic = self.__dict__.copy
+        my_dic = self.__dict__.copy()
         my_dic["__class__"] = self.__class__.__name__
+        """print these two var in iso format"""
         my_dic["created_at"] = self.created_at.isoformat()
         my_dic["updated_at"] = self.updated_at.isoformat()
         return my_dic
